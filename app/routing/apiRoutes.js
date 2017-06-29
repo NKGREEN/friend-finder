094eec979443f5b0c8f16e2c3c23d9daf5771736
Samuel Resende <sresende@knights.ucf.edu>
2017-06-28 22:14:54 -0400

    added additional logic for modal and began testing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,7 +12,7 @@ app.get ("/api/friends", function (req, res) {
 app.post("/api/friends", function(req, res) {
 	var friend = req.body;
 	friends.push(friend);
-	res.json(friend);
+	res.json(friends.match(friend));
 });
 
 };
