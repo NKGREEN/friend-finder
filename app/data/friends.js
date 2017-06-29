230ac18cf8dcfa0a5ec58ba462f37497be0f8fa7
Samuel Resende <sresende@knights.ucf.edu>
2017-06-28 20:47:36 -0400

    prelimnary logic on matching and posting

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,4 @@
+
 var friends = [
 {
 	"name": "Sam",
@@ -17,4 +18,33 @@ var friends = [
 
 
 ];
-module.exports = friends;
+exports.friends = friends;
+
+exports.findMatch = function (frand){
+	var differences = [];
+
+	for (var i = 0; i < friends.length; i++) {
+		var totalDifference = 0;
+
+		for (var j = 0; j < 10; j++) {
+			totalDifference += Math.abs(friends[i].scores[j] - frand.scores[j]);
+
+		}
+		differences.push(totalDifference);	
+	}
+	var index = 0;
+	var lowestScore = differences[0];
+
+	for (var i = 1; i < differences.length; i++) {
+		if (differences[i] < lowestScore) {
+			lowestScore = difference[i];
+			index = i;
+		}
+	}
+
+	var matchedFriend = friends[index];
+
+	return match;
+
+
+}
