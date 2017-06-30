46c67ab92cec43f281d78e3cb21e86082a17f5b5
Samuel Resende <sresende@knights.ucf.edu>
2017-06-30 10:23:08 -0400

    final touches

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,95 +1,84 @@
-
-var friends = [
-{
-	"name": "Leo Johnson",
-	"photo": "https://media.giphy.com/media/3ohzdCVQJuxOG4c716/giphy.gif",
-	"scores": [2, 5, 2, 4, 1, 2, 1, 4, 1, 5],
-	"bio": "I'm a real rager til I turn into a vegetable."
-},
-{
-	"name": "Laura Palmer",
-	"photo": "https://media.giphy.com/media/aTqjL0xAuEbmw/giphy.gif",
-	"scores": [5, 2, 2, 3, 5, 1, 4, 4, 3, 2],
-	"bio": "I'm lovely but not the most lively"
-},
-{
-	"name": "Audrey Horne",
-	"photo": "https://media.giphy.com/media/bZ8yF53offo7S/giphy.gif",
-	"scores": [3, 2, 2, 1, 5, 3, 3, 3, 5, 1],
-	"bio": "Hope you like dancin' ooh la la."
-},
-{
-	"name": "Shelly Johnson",
-	"photo": "https://media.giphy.com/media/xUPGcAetbrFHRh4mxq/giphy.gif",
-	"scores": [5, 1, 3, 1, 4, 2, 4, 3, 4, 3],
-	"bio": "I'm a great friend to have I've got all the pies. "
-},
-{
-	"name": "Donna Hayward",
-	"photo": "https://media.giphy.com/media/FHVUFHwv68UKY/giphy.gif",
-	"scores": [3, 2, 3, 4, 4, 3, 3, 2, 2, 3],
-	"bio": "I hope you're OK with feelings."
-},
-{
-	"name": "Dale Cooper",
-	"photo": "https://media.giphy.com/media/26mkhIj7fJHjq0JMI/giphy.gif",
-	"scores": [5, 1, 5, 1, 5, 1, 5, 1, 5, 1],
-	"bio": "I love solving a good murder, and a strong cup of coffee."
-},
-{
-	"name": "Bobby Briggs",
-	"photo": "https://media.giphy.com/media/3ohfFLa0tFRuJqZuPm/giphy.gif",
-	"scores": [3, 5, 4, 5, 3, 1, 1, 5, 2, 2],
-	"bio": "I'm an utter douchelord."
-},
-{
-	"name": "Norma Jennings",
-	"photo": "https://media.giphy.com/media/26n60J5FeXvCIumOY/giphy.gif",
-	"scores": [3, 1, 5, 2, 2, 3, 5, 1, 5, 5],
-	"bio": "I can make you breakfast!"
-},
-{
-	"name": "Log Lady",
-	"photo": "https://media.giphy.com/media/B9bjlOhhqUEdW/giphy.gif",
-	"scores": [1, 1, 1, 5, 1, 5, 2, 3, 3, 1],
-	"bio": "Hope you like threesomes! "
-},
-{
-	"name": "The Man From Another Place",
-	"photo": "https://media.giphy.com/media/Fujjgee5zjIZi/giphy.gif",
-	"scores": [5, 1, 3, 3, 5, 3, 3, 5, 5, 1],
-	"bio": "I have a feeling you are into some kinky stuff."
-},
+var friends = [{
+        "name": "Leo Johnson",
+        "photo": "https://media.giphy.com/media/3ohzdCVQJuxOG4c716/giphy.gif",
+        "scores": [2, 5, 2, 4, 1, 2, 1, 4, 1, 5],
+        "bio": "I'm a real rager til I turn into a vegetable."
+    }, {
+        "name": "Laura Palmer",
+        "photo": "https://media.giphy.com/media/aTqjL0xAuEbmw/giphy.gif",
+        "scores": [5, 2, 2, 3, 5, 1, 4, 4, 3, 2],
+        "bio": "I'm lovely but not the most lively"
+    }, {
+        "name": "Audrey Horne",
+        "photo": "https://media.giphy.com/media/bZ8yF53offo7S/giphy.gif",
+        "scores": [3, 2, 2, 1, 5, 3, 3, 3, 5, 1],
+        "bio": "Hope you like dancin' ooh la la."
+    }, {
+        "name": "Shelly Johnson",
+        "photo": "https://media.giphy.com/media/xUPGcAetbrFHRh4mxq/giphy.gif",
+        "scores": [5, 1, 3, 1, 4, 2, 4, 3, 4, 3],
+        "bio": "I'm a great friend to have I've got all the pies. "
+    }, {
+        "name": "Donna Hayward",
+        "photo": "https://media.giphy.com/media/FHVUFHwv68UKY/giphy.gif",
+        "scores": [3, 2, 3, 4, 4, 3, 3, 2, 2, 3],
+        "bio": "I hope you're OK with feelings."
+    }, {
+        "name": "Dale Cooper",
+        "photo": "https://media.giphy.com/media/26mkhIj7fJHjq0JMI/giphy.gif",
+        "scores": [5, 1, 5, 1, 5, 1, 5, 1, 5, 1],
+        "bio": "I love solving a good murder, and a strong cup of coffee."
+    }, {
+        "name": "Bobby Briggs",
+        "photo": "https://media.giphy.com/media/3ohfFLa0tFRuJqZuPm/giphy.gif",
+        "scores": [3, 5, 4, 5, 3, 1, 1, 5, 2, 2],
+        "bio": "I'm an utter douchelord."
+    }, {
+        "name": "Norma Jennings",
+        "photo": "https://media.giphy.com/media/26n60J5FeXvCIumOY/giphy.gif",
+        "scores": [3, 1, 5, 2, 2, 3, 5, 1, 5, 5],
+        "bio": "I can make you breakfast!"
+    }, {
+        "name": "Log Lady",
+        "photo": "https://media.giphy.com/media/B9bjlOhhqUEdW/giphy.gif",
+        "scores": [1, 1, 1, 5, 1, 5, 2, 3, 3, 1],
+        "bio": "Hope you like threesomes! "
+    }, {
+        "name": "The Man From Another Place",
+        "photo": "https://media.giphy.com/media/Fujjgee5zjIZi/giphy.gif",
+        "scores": [5, 1, 3, 3, 5, 3, 3, 5, 5, 1],
+        "bio": "I have a feeling you are into some kinky stuff."
+    },
 
 
 ];
 exports.friends = friends;
 
-exports.findMatch = function (frand){
-	var differences = [];
+exports.findMatch = function(frand) {
+    var differences = [];
 
-	for (var i = 0; i < friends.length; i++) {
-		var totalDifference = 0;
+    for (var i = 0; i < friends.length; i++) {
+        var totalDifference = 0;
 
-		for (var j = 0; j < 10; j++) {
-			totalDifference += Math.abs(friends[i].scores[j] - frand.scores[j]);
+        for (var j = 0; j < 10; j++) {
+            totalDifference += Math.abs(friends[i].scores[j] - frand.scores[j]);
 
-		}
-		differences.push(totalDifference);	
-	}
-	var index = 0;
-	var lowestScore = differences[0];
+        }
+        differences.push(totalDifference);
+    }
+    var index = 0;
+    var lowestScore = differences[0];
 
-	for (var i = 1; i < differences.length; i++) {
-		if (differences[i] < lowestScore) {
-			lowestScore = differences[i];
-			index = i;
-		}
-	}
+    for (var i = 1; i < differences.length; i++) {
+        if (differences[i] < lowestScore) {
+            lowestScore = differences[i];
+            index = i;
+        }
+    }
 
-	var matchedFriend = friends[index];
+    var matchedFriend = friends[index];
 
-	return matchedFriend;
+    return matchedFriend;
 
 
 }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,14 +4,14 @@ var path = require("path");
 
 module.exports = function(app) {
 
-app.get ("/api/friends", function (req, res) {
-		res.json(friends.friends);
-});
+    app.get("/api/friends", function(req, res) {
+        res.json(friends.friends);
+    });
 
-app.post("/api/friends", function(req, res) {
-	var friend = req.body;
-	res.json(friends.findMatch(friend));
-	friends.friends.push(friend);
-});
+    app.post("/api/friends", function(req, res) {
+        var friend = req.body;
+        res.json(friends.findMatch(friend));
+        friends.friends.push(friend);
+    });
 
 };
